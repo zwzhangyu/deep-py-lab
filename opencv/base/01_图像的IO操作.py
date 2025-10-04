82ebdfce765c6d53fb66bdbe596c9ee1f378fed8
zhangyu <zhangyu_techlife@163>
2025-10-04 21:20:42 +0800

    init

--- opencv/base/01_图像的IO操作.py
+++ opencv/base/01_图像的IO操作.py
@@ -1,9 +1,13 @@
 import numpy as np
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 # 读取图像
 img = cv.imread("images/avatar.jpg", cv.IMREAD_GRAYSCALE)
 
 # 显示图像
-cv.imshow("img", img)
-cv.waitKey(0)
+# cv.imshow("img", img)
+# cv.waitKey(0)
+
+plt.imshow(img, cmap="gray")
+plt.show()
